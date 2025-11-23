c53098068a7aa03ff1dab8a14475f037f6346232
amarquez28 <zhukow28@gmail.com>
2025-11-22 17:45:52 -0700

    some minor tweaks

--- BillLib.c
+++ BillLib.c
@@ -1,30 +1,31 @@
 
+#include "BillLib.h"
 
+char paymentMethod[ACCEPTEDMETHODS][15] = {
+    "Apple Pay ", "Google Pay", "Money Order ", "Cash ", "Card ", "Klarna "};
 
-void filter_bills(sBill_LL *list, int criteria){
-    switch (criteria)
-    {
-    case BY_AMOUNT:
-        /* code */
-        break;
-    case BY_DATE:
-        /* code */
-        break;
-    default:
-        break;
-    }
+void filter_bills(sBill_LL *list, int criteria) {
+  switch (criteria) {
+  case BY_AMOUNT:
+    /* code */
+    break;
+  case BY_DATE:
+    /* code */
+    break;
+  default:
+    break;
+  }
 }
 
-void sort_bills(sBill_LL *list, int field);{
-    switch (field)
-    {
-    case AMOUNT:
-        /* code */
-        break;
-    case DATE;
-        /* code */
-        break;
-    default:
-        break;
-    }
+void sort_bills(sBill_LL *list, int field) {
+  switch (field) {
+  case AMOUNT:
+    /* code */
+    break;
+  case DATE:
+    /* code */
+    break;
+  default:
+    break;
+  }
 }
--- BillLib.h
+++ BillLib.h
@@ -1,60 +1,55 @@
 #ifndef BILLLIB_H
 #define BILLLIB_H
+
+#include <stdio.h>
+
 #define ACCEPTEDMETHODS 6
+#define MAX_NAME_LEN 20
+#define MAX_NOTE_LEN 50
+#define DATE_LEN 11 // 10 chars + null terminator
 
-enum paymentType{CREDIT, DEDIT, MONEYORDER, CASH, CHECK, PAYMENTPLANS};
+enum paymentType { CREDIT, DEBIT, MONEYORDER, CASH, CHECK, PAYMENTPLANS };
 
-enum BillCriteria{BY_AMOUNT,BY_DATE};//TODO: ADD MORE CRITERIA
+enum BillCriteria { BY_AMOUNT, BY_DATE }; // TODO: ADD MORE CRITERIA
 
-enum sortField{AMOUNT,DATE};//TODO: ADD MORE CRITERIA
+enum sortField { AMOUNT, DATE }; // TODO: ADD MORE CRITERIA
 
+extern char paymentMethod[ACCEPTEDMETHODS][15];
 
-char paymentMethod[ACCEPTEDMETHODS][15] = {
-    "Apple Pay ",
-    "Google Pay",
-    "Money Order ",
-    "Cash ",
-    "Card ",
-    "Klarna "
-};
+typedef struct sBill {
+  char category[MAX_NAME_LEN];
+  char provider[MAX_NAME_LEN];
+  float total;
+  enum paymentType payment;
+  char paymentMethod[15];
+  char date[DATE_LEN];
+  char billingPeriod[MAX_NAME_LEN];
+  int taxRelevance;
+  char notes[MAX_NOTE_LEN];
+  sBill *next;
+  sBill *prev;
 
-typedef struct{
-    char category [20];
-    char provider [20];
-    float total;
-    enum paymentType payment;
-    char paymentMethod[15];
-    char date[10];
-    char billingPeriod[20];
-    int taxRelevance;
-    char notes[50];
-    sBill *next;
-    sBill *prev;
+} sBill;
 
-}sBill;
+typedef struct {
+  sBill *head;
+  sBill *tail;
+  int size;
 
-typedef struct{
-    sBill *head;
-    sBill *tail;
-    int size;
-
-}sBill_LL;
-
+} sBill_LL;
 
 void init_list(sBill_LL *list);
-void add_bill(sBill_LL *list, sBill newBill);
+void add_bill(sBill_LL *list, sBill *newBill);
 void edit_bill(sBill_LL *list, int index);
 void delete_bill(sBill_LL *list, int index);
 void filter_bills(sBill_LL *list, int criteria);
 void sort_bills(sBill_LL *list, int field);
-void generate_report(sBill_LL *list);
-void save_bill(sBill_LL *list);
-void load_bill(sBill_LL *list);
-void export_csv(sBill_LL *list);
-void save_bin(sBill_LL *list);
-void load_bin(sBill_LL *list);
-void print_menu(sBill_LL *list);
+void generate_report(const sBill_LL *list);
+void save_bill(const sBill_LL *list);
+void load_bill(const sBill_LL *list);
+void export_csv(const sBill_LL *list);
+void save_bin(const sBill_LL *list);
+void load_bin(const sBill_LL *list);
+void print_menu(const sBill_LL *list);
 
-
-
-#endif //BILLLIB_H
+#endif // BILLLIB_H
